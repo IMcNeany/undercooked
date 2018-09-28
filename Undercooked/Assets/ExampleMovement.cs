1c257d12fd27ab2aeb88ef80b2c7da2a1e8d882a
Sam Hibb <samhibbert13@yahoo.co.uk>
2018-09-28 13:20:55 +0100

    adding exampleMovement

---
+++ Undercooked/Assets/ExampleMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleMovement : MonoBehaviour {
+
+    public int playerNumber;
+    
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float moveHorizontal = Input.GetAxis("Horizontal_" + playerNumber);
+
+        float moveVertical = Input.GetAxis("Vertical_" + playerNumber);
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        GetComponent<Rigidbody2D>().AddForce(movement * 5.0f);
+
+        if (Input.GetButton("Action_" + playerNumber)) {
+            Debug.Log("|||||||||||||||||||||||ACTION!|||||||||||||||||||");
+        }
+
+    }
+}
