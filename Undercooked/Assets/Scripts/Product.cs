1cc91ee442906cb7ae8df9d355383efe885eb813
Unknown <Elliot2.martin@live.uwe.ac.uk>
2018-09-28 09:55:03 +0100

    Separated food items and cooking utensils, added placement functions for each type

--- Undercooked/Assets/Scripts/Product.cs
+++ Undercooked/Assets/Scripts/Product.cs
@@ -4,11 +4,10 @@ using UnityEngine;
 
 public class Product : Pickup {
 
-    public List<Item> item_list;
-    public Sprite product_sprite;
+    public List<FoodItem> item_list;
 
 
-    public void AddItem(ItemType type)
+    public void AddItem(FoodType type)
     {
         for (int i = 0; i < item_list.Count; i++)
         {
@@ -24,6 +23,23 @@ public class Product : Pickup {
 
     public override void PlaceObject(GameObject obj)
     {
+        //to do add utensil stuff
         base.PlaceObject(obj);
+
+        if(obj.GetComponent<FoodItem>())
+        {
+            FoodItem obj_item = obj.GetComponent<FoodItem>();
+            if(obj_item.prepared)
+            {
+                AddItem(obj_item.type);
+                Destroy(obj);
+            }
+            //if obj has an item, add item to the current product and place product on obj instead
+        }
+        else
+        {
+            //place product on counter/goal
+            transform.position = obj.transform.position;
+        }
     }
 }
