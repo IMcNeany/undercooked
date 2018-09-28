40eb08e4c75e7f4a3bbd6f733112462d7c00eb3c
Unknown <Elliot2.martin@live.uwe.ac.uk>
2018-09-28 09:22:20 +0100

    Added Base pickup items and product

---
+++ Undercooked/Assets/Scripts/Product.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Product : Pickup {
+
+    public List<Item> item_list;
+    public Sprite product_sprite;
+
+
+    public void AddItem(ItemType type)
+    {
+        for (int i = 0; i < item_list.Count; i++)
+        {
+            //check for duplicates and that item is prepared
+            
+        }
+    }
+
+    public void RefreshSprite()
+    {
+        //loop through list and set sprite depending on its contents
+    }
+
+    public override void PlaceObject(GameObject obj)
+    {
+        base.PlaceObject(obj);
+    }
+}
