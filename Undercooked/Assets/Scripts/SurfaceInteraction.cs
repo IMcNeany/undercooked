952994dfe81f18375ebb7519073afa533cf17a21
Isobel McNeany <icmc112@yahoo.co.uk>
2018-09-28 10:08:54 +0100

    Commit of unity project also simple movement and pick up mechanic

---
+++ Undercooked/Assets/Scripts/SurfaceInteraction.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SurfaceInteraction : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("player entered");
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("player on");
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("player exited");
+    }
+}
